37daf6ef72a1509998b3c22165c9841c56e284a2
BugHunter-X <askzenai@gmail.com>
2025-02-05 19:12:06 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,10 @@
+proc get_element {list index} {
+  if {$index < 0 || $index >= [llength $list]} {
+    return -code error "Index out of range" 
+  }
+  return [lindex $list $index]
+}
+
+set mylist {a b c d e}
+puts [get_element $mylist 10]   ;# Returns error message
+puts [get_element $mylist 2]    ;# Returns "c"
